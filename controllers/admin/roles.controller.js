ac229e02e60a4d4429627a41ec201084b768a85d
Ho Hung <hohung1203@gmail.com>
2024-01-04 10:32:51 +0700

    add function of authorized(permission)

--- controllers/admin/roles.controller.js
+++ controllers/admin/roles.controller.js
@@ -3,10 +3,11 @@ const rolesModel = require('../../models/role.model')
 const systemConfig = require('../../config/system/index')
 
 
-// [GET] /admin/
+// [GET] /admin/roles
 module.exports.index = async (req, res) => {
 
   const group = await rolesModel.find({
+    deleted: false
 
   })
 
@@ -16,17 +17,97 @@ module.exports.index = async (req, res) => {
   }) 
 }
 
-// [GET] /admin/create
+// [GET] /admin/roles/create
 module.exports.create = async (req, res) => {
   res.render(`${systemConfig.prefixAdmin}/pages/roles/create`, {
-    titlePage: 'Roles'
+    titlePage: 'Roles create'
     ,group: []
   }) 
 }
 
-// [POST] /admin/create
+// [POST] /admin/roles/create
 module.exports.createPost= async (req, res) => {
   await rolesModel.create(req.body)
-  req.flash('success', 'Thêm mới thành công')
+  req.flash('changeSuccess', 'Thêm mới thành công')
   res.redirect(`/${systemConfig.prefixAdmin}/roles`)
 }
+
+// [DELETE] /admin/create/roles/delete/:id
+module.exports.deleteItem = async (req, res) => {
+  const id = req.params.id
+
+  await rolesModel.updateOne(
+      { _id: id }, 
+      { deleted: true, deletedAt: new Date() }
+  )
+
+  req.flash('changeSuccess', 'Sản phẩm đã được xoá thành công!')
+
+  res.redirect('back')
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+
+  const id = req.params.id
+  const record = await rolesModel.findOne({
+    _id: id
+  })
+
+  res.render(`${systemConfig.prefixAdmin}/pages/roles/edit`, {
+    titlePage: 'Roles edit'
+    ,record
+  }) 
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id
+  await rolesModel.findOneAndUpdate({
+    _id: id
+  },
+    req.body
+  )
+  req.flash('changeSuccess', 'Chỉnh sửa thành công')
+  res.redirect(`/${systemConfig.prefixAdmin}/roles`)
+}
+
+// [GET] /admin/roles/permissions
+module.exports.permissions = async (req, res) => {
+
+  const records = await rolesModel.find({
+    deleted: false
+  })
+
+  res.render(`${systemConfig.prefixAdmin}/pages/roles/permissions`, {
+    titlePage: 'Roles permissions',
+    records
+
+  }) 
+}
+
+// [PATCH] /admin/roles/permissions
+module.exports.permissionsPatch = async (req, res) => {
+  try {
+    const permissions = JSON.parse(req.body.permissions)
+    
+    permissions.forEach(async item => {
+      await rolesModel.findOneAndUpdate(
+        {
+        _id: item.id
+      }, 
+        {
+        permissions: item.permissions
+      }
+      )
+    })
+    req.flash('changeSuccess', 'Cập nhật quyền mới thành công!')
+    res.redirect('back')
+
+  }
+  catch(err) {
+    req.flash('changeError', 'Cập nhật quyền mới KHÔNG thành công!')
+  }
+}
+
+
